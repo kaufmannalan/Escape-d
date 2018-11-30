3afa3e35c0ab585b41eccaa2641a67f818103240
kaufmannalan <42677923+kaufmannalan@users.noreply.github.com>
2018-11-30 01:30:33 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
         public static Scanner scan;
         public static int turns = 30;
-        public static String output = "As you inspect the dimly lit foyer around yourself, you notice a caste iron door to your north with a brass knocker and engraved symbols.\n To your west lies a stout oaken bench with blood red cushions, it is pushed against the wall.\n To the east is plain ebony chest with a raven carved into it. \nIn the center of the room lies a large wax candle. The candle is unlit, but it looks like it has been lit before.\n";
+        public static String output = "As you inspect the dimly lit foyer around yourself, you notice a caste iron door to your north with a brass knocker and engraved symbols.\n To your west lies a stout oaken bench with blood red cushions, it is pushed against the wall.\n To the east is plain ebony chest with a raven carved into it. \n In the center of the room lies a large wax candle. The candle is unlit, but it looks like it has been lit before.\n";
 
 
         public static boolean inFoyer = true;
@@ -18,18 +18,22 @@ import java.util.Scanner;
         public static boolean chestOpen = false;
         public static boolean hasMatches = false;
         public static boolean candleLit = false;
-        public static boolean redDoorLocked = true;
-        public static boolean redDoorOpen = false;
+        public static boolean DoorLocked2 = true;
+        public static boolean DoorOpen2 = false;
 
         public static boolean getPen = false;
         public static boolean writeBook = true;
-        public static boolean blueDoorLocked = true;
-        public static boolean blueDoorOpen = false ;
+        public static boolean DoorLocked3 = true;
+        public static boolean DoorOpen3 = false ;
 
         public static boolean trumpetAcquired = false;
         public static boolean playTrumpet = false;
         public static boolean playPiano = false;
         public static boolean playDrum = false;
+        public static boolean DoorLocked4 = true;
+        public static boolean DoorOpen4 = false ;
+        public static boolean goDoor4 = false;
+        public static boolean wentDoor = false;
 
 
         public static void main(String[] args) {
@@ -40,16 +44,18 @@ import java.util.Scanner;
         for (int i = 1; i < 31; i++) {
         playGame();
         turns--;
-        if (playTrumpet && playPiano && playDrum) {
+        if (wentDoor) {
         break;
         }
         }
 
-        if (playTrumpet && playPiano && playDrum) {
-        System.out.printf("\nSuccess! The door to the north opens and you escape the conservatory, exiting the mansion with %d spare turns.", turns);
+        if (wentDoor)
+
+        {
+            System.out.printf("\nSuccess! You're outside and away from danger. \nYou have escaped the Baron's mansion with %d spare turns, but will you be as lucky next time?", turns);
         }
         else {
-        System.out.printf("\nYou see a tall dark figure enter the room. You have failed the escape room in time!");
+        System.out.printf("\nYou see a dark cackling figure enter the door. You have been discovered and failed to escape in time!");
         }
         }
 
@@ -101,7 +107,7 @@ import java.util.Scanner;
 
 
         case "look chest":
-        output = "You see a plain, ebony chest with the engraving of a raven resting on the floor. Although it is closed there doesn't seem to be a lock.\n";
+        output = "You see a plain, ebony chest resting on the floor. It has the engraving of a raven resting on the floor. Although it is closed there doesn't seem to be a lock.\n";
         break;
 
         case "open chest":
@@ -123,17 +129,17 @@ import java.util.Scanner;
         case "light candle":
         if (hasMatches) {
         candleLit = true;
-        redDoorLocked = false;
-        output = "You light the candle with a match. Suddenly, the runes along the wax of the candle brighten as its flame flickers. You fell a strong gust behind you that seems to make the candle blow brighter. You hear a loud metal CLICK coming from the direction of the door.\n";
+        DoorLocked2 = false;
+        output = "You light the candle with a match. Suddenly, the runes along the wax of the candle brighten as its flame flickers. You feel a strong gust behind you that seems to make the candle blow brighter. You hear a loud metal CLICK coming from the direction of the door.\n";
         }
         else {
-        output = "You cannot light the candle without a match or tinder:(\n";
+        output = "You cannot light the candle without a match or tinder.\n";
         }
         break;
 
         case "look door":
-        if (!redDoorLocked) {
-        if (redDoorOpen) {
+        if (!DoorLocked2) {
+        if (DoorOpen2) {
         output = "The black iron door to the north is unlocked and open.\n";
         }
         else {
@@ -141,19 +147,19 @@ import java.util.Scanner;
         }
         }
         else {
-        output = "The black iron door to the north is locked :(\n";
+        output = "The black iron door to the north is locked\n";
         }
         break;
 
         case "open door":
-        if (!redDoorLocked) {
-        redDoorOpen = true;
+        if (!DoorLocked2) {
+        DoorOpen2 = true;
         output = "The door has been opened.\n";
         }
         break;
 
         case "go north":
-        if (redDoorOpen) {
+        if (DoorOpen2) {
         inFoyer = false;
         inLibrary = true;
         output = "You exit the foyer, and the door slams loudly behind you with a CLICK. It doesn't seem to be openable again.\n You now find yourself in a small library with a single shelf of books. There is a mahogany desk in the center of the room. Although it seems like the desk is meant for studying, there are large scratch marks running along it and its corners have been gnawed off.\n There lies a pen and scroll on top of it.\n To your north, there is a similar caste iron door with a copper knocker. It seems to have different symbols engraved into it.\n";
@@ -189,14 +195,32 @@ import java.util.Scanner;
         break;
 
         case "look":
-        output = "You exit the foyer, and the door slams loudly behind you with a CLICK. It doesn't seem to be openable again.\n You now find yourself in a small library with a single shelf of books. There is a mahogany desk in the center of the room. Although it seems like the desk is meant for studying, there are large scratch marks running along it and its corners have been gnawed off.\n There lies a pen and scroll on top of it.\n To your north, there is a similar caste iron door with a copper knocker. It seems to have different symbols engraved into it.\n";
+        output = "You exit the foyer, and the door slams loudly behind you with a CLICK. It doesn't seem to be openable again.\nYou now find yourself in a small library with a single shelf of books. There is a mahogany desk in the center of the room. Although it seems like the desk is meant for studying, there are large scratch marks running along it and its corners have been gnawed off.\nThere lies a pen and scroll on top of it.\nTo your north, there is a similar caste iron door with a copper knocker. It seems to have different symbols engraved into it.\n";
         break;
 
         case "look shelves":
+        output= "When you search the bookshelf, you see an open book with the markings of a wolf. It is titled \"The Autobiography of...\" with the rest of the title missing.\n";
+        break;
+
+        case "look shelf":
         output= "When you search the bookshelf, you see an open book with the markings of a wolf of the shelf. It is titled \"The Autobiography of...\" with the rest of the title missing.\n";
         break;
 
-        case "get pen":
+        case "look book":
+        output= "You see an open book with the markings of a wolf. It is titled \"The Autobiography of...\" with the rest of the title missing.\n";
+        break;
+
+        case "look pen":
+            output= "You see a slick black fountain pain with a red ink streak along its side.\n";
+            break;
+
+
+        case "look scroll":
+            output= "You see an open book with the markings of a wolf of the shelf. It is titled \"The Autobiography of...\" with the rest of the title missing.\n";
+            break;
+
+
+            case "get pen":
         getPen = true;
         output = "You picked up the pen from the desk.\n It's leaking a little bit of ink\n";
         break;
@@ -207,13 +231,13 @@ import java.util.Scanner;
 
         case "write book":
         writeBook = true;
-        blueDoorLocked = false;
+        DoorLocked3 = false;
         output = "You have completed the autobiography! You then hear a loud CLICK from your north.\n";
         break;
 
         case "look door":
-        if (!blueDoorLocked) {
-        if (blueDoorOpen) {
+        if (!DoorLocked3) {
+        if (DoorOpen3) {
         output = "The iron door to the north is unlocked and open.\n";
         }
         else {
@@ -221,19 +245,19 @@ import java.util.Scanner;
         }
         }
         else {
-        output = "The iron door to the north is locked :(\n";
+        output = "The iron door to the north is locked \n";
         }
         break;
 
         case "open door":
-        if (!blueDoorLocked) {
-        blueDoorOpen = true;
+        if (!DoorLocked3) {
+        DoorOpen3 = true;
         output = "The door has been opened.\n";
         }
         break;
 
         case "go north":
-        if (blueDoorOpen) {
+        if (DoorOpen3) {
         inLibrary = false;
         inConservatory = true;
         output = "As you leave the small study, and the door slams loudly behind and then resounds with a loud CLICK. It seems to have locked automatically, so you cannot return to the library.\n You now find yourself in the large conservatory. There is another black to your north. It has a platinum knocker and the unmistakable symbol of the Baron's sigil.\n There are several broken down instruments. The only three that seem to function are a grand piano with a red velvet bench, and silvered trumpet, and a set of bongo drums with a taut skin. \n There's also a stand with sheet music in the center of the room.\n";
@@ -281,47 +305,74 @@ import java.util.Scanner;
         break;
 
         case "play trumpet":
-        if (!playPiano && !playDrum && trumpetAcquired) {
+        if (!playPiano && !playDrum) {
         playTrumpet = true;
         output = "With a triumphant TOOT, the trumpet plays.\n";
         }
-        else {
-        output = "Did you get the trumpet yet? \n";
-        }
+
         break;
 
-        //player may not know to add this step because "go piano" sounds weird
+
         case "go piano":
         output = "The piano bench's cushion is kind of squishy\n";
         break;
 
         case "play piano":
-        if (playTrumpet && !playDrum) {
+        if (playTrumpet && !playDrum && !playPiano) {
         playPiano = true;
         output = "You play the piano Goonies style, it seems to be very out of tune.\n";
         }
         else {
-        output = "Seems like your sense of order might be off...\n";
+        playPiano = false;
+        output = "I guess the stars haven't aligned...\n";
         }
         break;
 
-        //player may not know to add this step because "go drum" sounds weird
+
         case "go drum":
-        output = "the drum's skin is coarse and uncomftorble to touch.\n";
+        output = "the drum's skin is coarse and uncomfortable to touch.\n";
         break;
 
         case "play drum":
         if (playTrumpet && playPiano) {
         playDrum = true;
-        output = "You're not quite sure how to play the drums, but you tap on them anyway.\n";
+        output = "Once you lightly tap on the drum, you feel the room quake suddenly and a CLICK resounds from the door.\n";
         }
         else {
-        output = "I guess the stars haven't aligned....\n";
+        output = "Nothing happened... You better hurry up before time runs out!\n";
         }
         break;
 
+        case "go door":
+        output = "You go to the door";
+        break;
+
+        case "open door":
+        if (DoorLocked4 == true) {
+            output = "It's locked";
+        }
+        else {
+            output = "You see light trickle from the outside as you slowly push the heavy black iron door open.";
+            DoorOpen4 = true;
+        }
+        break;
+
+        case "go north":
+       if (DoorOpen4) {
+           wentDoor = true;
+
+       }
+       else {
+           output = "You can't";
+       }
+       break;
+
+
         default:
         output = "In the conservatory there is a large door to the north that is locked. There are three instruments- a trumpet, a piano, and a drum. There is a sheet of music on a stand.\n";
+        }
+        if (playTrumpet && playPiano && playDrum) {
+            DoorLocked4 = false;
         }
         }
         }
